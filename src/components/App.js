e5a82375c3e8d5d67a3d636d89db9cfbc20d656f
Bridget Kelly <bckelly197@gmail.com>
2022-03-01 11:26:35 -0500

    commit tree 144e294fc3213b30988fa2b621f3ba4f587329be

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App () {
+    const [onDogImage, setOnDogImage] = useState('');
+
+    useEffect (() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(resp => resp.json())
+        .then(dogData => setOnDogImage(dogData.message));
+    }, []);
+
+    if (!onDogImage) {
+        return <p>...Loading</p>;
+    } 
+
+    return (
+        <img src={onDogImage} alt="A Random Dog" />
+    )
+}
+
+export default App;
